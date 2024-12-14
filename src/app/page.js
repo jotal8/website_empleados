f413bd583e7fc3ec2e76c3a491ed4b53ef142e38
Julian Otalvaro <jotal8@gmail.com>
2024-12-14 21:27:18 +0000

    Se va organizando el formulario para el login

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,37 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        Bienvenidos al sistema de gestión de empleados!
-      </div>
-    </main>
-  )
+  const a = 2;
+
+  if(a == 1){
+    return (
+      <main className={styles.main}>
+        <div className={styles.description}>
+          Estas loggeado!!!
+        </div>
+      </main>
+    )
+  }else{
+    return (
+      <main className={styles.main}>
+        <div className={styles.description}>
+          Estas loggeado!!!Bienvenidos al sistema de gestión de empleados!
+        </div>
+        <form className={styles.formLogin}>
+            <p>
+              <input type="text" name="email" />
+            </p>
+
+            <p>
+              <input type="password" name="password" />
+            </p>
+  
+            <div className={styles.buttonContainer}>
+              <button className={styles.ingresarBtn}>Ingresar</button>
+            </div>
+          </form>
+      </main>
+    )
+  }
+    
 }
